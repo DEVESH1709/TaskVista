0df7c1b7aa04881920acbeb1559a83dbdea43fbf
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-02-20 19:04:08 +0530

    add some changes

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import morgan from "morgan";
 import { errorHandler, routeNotFound } from "./middlewares/errorMiddlewaves.js";
 import routes from "./routes/index.js";
 import { dbConnection } from "./utils/index.js";
-import path from "path"
+
 dotenv.config();
 
 dbConnection();
@@ -14,11 +14,6 @@ dbConnection();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
-const _dirname =path.resolve();
-
-
-
 
 app.use(
   cors({
@@ -38,11 +33,5 @@ app.use("/api", routes);
 
 app.use(routeNotFound);
 app.use(errorHandler);
-
-app.use(express.static(path.join(_dirname,"/client/dist")));
-
-app.get('*',(req,res=>{
-  res.sendFile(path.resolve(_dirname,"client","dist","index.html"));
-}))
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
